fd658e4e5f0d8366f4216771def2c8bd56cdeed1
fullstackn <fullstackn@outlook.com>
2024-07-23 07:29:10 +0300

    url

--- rxconfig.py
+++ rxconfig.py
@@ -2,5 +2,5 @@ import reflex as rx
 
 config = rx.Config(
     app_name="reflex_example",
-    api_url="https://reflex-example-fullstackn.onrender.com:8000/",
+    api_url="https://reflex-example-fullstackn.onrender.com:8000",
 )
